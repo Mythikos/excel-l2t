4b1277cfa493219dc2e74be09e3cdfae1b867f37
Vincent Lakatos <pntbllmstr74@aol.com>
2018-12-23 00:40:52 -0800

    Initial commit

---
+++ excel-l2t/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excel_load_to_text
+{
+    public static class Constants
+    {
+        public static class Application
+        {
+            public static readonly string Name = "Excel L2T";
+            public static readonly string Version = "0.0.1";
+        }
+    }
+}
